4aeb14d6291f122ca07cb304c3641ef8b883c9a9
152Voyage <2042700934@qq.com>
2026-05-31 10:49:31 +0800

    实验1：客户端的send发送数量超出服务端的receive

--- socket_client.py
+++ socket_client.py
@@ -2,6 +2,6 @@ import socket
 #创建客户端对象
 client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(('127.0.0.1',8888))
-#向服务端发送
-date="hello tcp/ip"
+#向服务端发送 一个哇是3字节，1000个就是三千字节
+date="哇"*1000
 client.send(date.encode('utf-8'))
